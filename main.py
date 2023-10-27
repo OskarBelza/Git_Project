d759ef3993783f3a6e7be92de3a510f1ccd5a5e4
GrzegorzDrozdz <grzegorz.drozdz@edu.uekat.pl>
2023-10-27 15:05:31 +0200

    creating histogram (part 2)

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-print("hello")
 zdanie = input("podaj ciąg znaków z którego zostanie utworzony histogram: ")
+
 histogram ={}
+
 for litera in zdanie:
     if litera.isalpha():
         litera.lower()
         if litera in histogram:
+            histogram[litera]+=1
+        else:
+            histogram[litera]=1
