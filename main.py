cab82182517fba533c5f9c2b60e8dac98f994a8d
GrzegorzDrozdz <grzegorz.drozdz@edu.uekat.pl>
2023-10-27 15:03:48 +0200

    creating histogram (in progress)

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("hello")
+zdanie = input("podaj ciąg znaków z którego zostanie utworzony histogram: ")
+histogram ={}
+for litera in zdanie:
+    if litera.isalpha():
+        litera.lower()
+        if litera in histogram:
