22044c6a99c7af402b9953ece253a43ffdffe471
OskarBelza <oskar.belza@edu.uekat.pl>
2023-10-28 16:02:38 +0200

    requested changes

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
 import requests
 
+
 def creating_histogram(answer):
     histogram = {}
-    for litera in zdanie:
-        if litera.isalpha():
-            litera.lower()
-            if litera in histogram:
-                histogram[litera] +=1
+    for letter in answer:
+        if letter.isalpha():
+            letter.lower()
+            if letter in histogram:
+                histogram[letter] += 1
             else:
-                histogram[litera]=1
+                histogram[letter] = 1
     return histogram
+
 
 def text_source():
     print("1 - Wpisz z klawiatury\n2 - Podaj adres URL")
     source = input("Wybierz opcje: ")
     if source == "1":
-        zdanie = input("Podaj zdanie")
-        return zdanie
+        text = input("Podaj zdanie")
+        return text
     elif source == "2":
         url = input("Podaj adres URL: ")
         response = requests.get(url)
-        return response.text
+        if response.status_code == 200:
+            return response.text
+        else:
+            print("Bład pobierania strony")
+            return None
     else:
         print("Niepoprawny wybor")
+        return None
+
 
 while True:
     print("1 - Generuj histogram \n2 - Wyjście")
-    wybor = input("WYbierz opcje: ")
-
-    if wybor == "1":
-        zdanie = input("Podaj zdanie: ")
-        histogram = creating_histogram(zdanie)
-        for litera, licznik in histogram.items():
-            print(f"{litera}: {licznik}")
-    elif wybor == "2":
+    choice = input("WYbierz opcje: ")
+    if choice == "1":
+        text = text_source()
+        if text:
+            histogram = creating_histogram(text)
+            for letter, counter in histogram.items():
+                print(f"{letter}: {counter}")
+    elif choice == "2":
         break
     else:
         print("Niepoprawny wybor")
