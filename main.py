7550a818b78a24be2ca0098296c32367c689961e
OskarBelza <oskar.belza@edu.uekat.pl>
2023-10-28 14:37:58 +0200

    addition of Url

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+
 def creating_histogram(answer):
     histogram = {}
     for litera in zdanie:
@@ -9,6 +11,18 @@ def creating_histogram(answer):
                 histogram[litera]=1
     return histogram
 
+def text_source():
+    print("1 - Wpisz z klawiatury\n2 - Podaj adres URL")
+    source = input("Wybierz opcje: ")
+    if source == "1":
+        zdanie = input("Podaj zdanie")
+        return zdanie
+    elif source == "2":
+        url = input("Podaj adres URL: ")
+        response = requests.get(url)
+        return response.text
+    else:
+        print("Niepoprawny wybor")
 
 while True:
     print("1 - Generuj histogram \n2 - Wyjście")
