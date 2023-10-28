a24a95d2a34d217968ff9113edf49479067b5fee
OskarBelza <oskar.belza@edu.uekat.pl>
2023-10-28 14:28:48 +0200

    Moving creation of histogram to function

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-histogram ={}
+def creating_histogram(answer):
+    histogram = {}
+    for litera in zdanie:
+        if litera.isalpha():
+            litera.lower()
+            if litera in histogram:
+                histogram[litera] +=1
+            else:
+                histogram[litera]=1
+    return histogram
 
-"""for litera in zdanie:
-    if litera.isalpha():
-        litera.lower()
-        if litera in histogram:
-            histogram[litera]+=1
-        else:
-            histogram[litera]=1
-
-print("Histogram częstości występowania liter: ")
-for litera, licznik in histogram.items():
-    print(f"{litera}: {licznik}")"""
 
 while True:
     print("1 - Generuj histogram \n2 - Wyjście")
@@ -18,7 +16,7 @@ while True:
 
     if wybor == "1":
         zdanie = input("Podaj zdanie: ")
-        histogram =  # Przeniesc tworzenie histogramu do funkcji
+        histogram = creating_histogram(zdanie)
         for litera, licznik in histogram.items():
             print(f"{litera}: {licznik}")
     elif wybor == "2":
