4ceb8c814a370349553ae6e69e0eec71ae381e32
OskarBelza <oskar.belza@edu.uekat.pl>
2023-10-28 14:24:01 +0200

    Creating menu for histogram

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-zdanie = input("podaj ciąg znaków z którego zostanie utworzony histogram: ")
-
 histogram ={}
 
-for litera in zdanie:
+"""for litera in zdanie:
     if litera.isalpha():
         litera.lower()
         if litera in histogram:
@@ -12,4 +10,18 @@ for litera in zdanie:
 
 print("Histogram częstości występowania liter: ")
 for litera, licznik in histogram.items():
-    print(f"{litera}: {licznik}")
+    print(f"{litera}: {licznik}")"""
+
+while True:
+    print("1 - Generuj histogram \n2 - Wyjście")
+    wybor = input("WYbierz opcje: ")
+
+    if wybor == "1":
+        zdanie = input("Podaj zdanie: ")
+        histogram =  # Przeniesc tworzenie histogramu do funkcji
+        for litera, licznik in histogram.items():
+            print(f"{litera}: {licznik}")
+    elif wybor == "2":
+        break
+    else:
+        print("Niepoprawny wybor")
